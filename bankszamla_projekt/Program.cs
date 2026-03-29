55f51f7b75bdda34f69878c58fdb645a6cc86392
aandr1s <b3domand@vasvari.org>
2026-03-29 22:03:18 +0200

    menu kiiratasa

--- bankszamla_projekt/Program.cs
+++ bankszamla_projekt/Program.cs
@@ -13,7 +13,23 @@ namespace bankszamla_projekt
         static void Main(string[] args)
         {
 
+            bool kilep = false;
+            while (!kilep)
+            {
+                Console.Clear();
+                Console.WriteLine("=== EGYSZERŰ BANKI RENDSZER ===");
+                Console.WriteLine("1. Számlák listázása");
+                Console.WriteLine("2. Befizetés");
+                Console.WriteLine("3. Kifizetés");
+                Console.WriteLine("4. Utalás");
+                Console.WriteLine("5. Hitelkeret módosítása");
+                Console.WriteLine("6. Kilépés és Naplózás");
+                Console.Write("\nVálasszon egy menüpontot: ");
 
-        }
+                string valasztas = Console.ReadLine();
+
+
+            }
+        }      
     }
 }
