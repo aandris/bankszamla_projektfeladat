09393087883c98f6ee0b0d355c9b8f018afee02e
aandr1s <b3domand@vasvari.org>
2026-03-30 21:05:04 +0200

    Adatbeolvasas

--- bankszamla_projekt/Program.cs
+++ bankszamla_projekt/Program.cs
@@ -10,8 +10,13 @@ namespace bankszamla_projekt
 {
     internal class Program
     {
+
+        static List<Account> szamlak = new List<Account>();
+
         static void Main(string[] args)
         {
+
+            AdatokBetoltese("szamlak.txt");
 
             bool kilep = false;
             while (!kilep)
@@ -21,7 +26,7 @@ namespace bankszamla_projekt
                 Console.WriteLine("1. Számlák listázása");
                 Console.WriteLine("2. Befizetés");
                 Console.WriteLine("3. Kifizetés");
-                Console.WriteLine("4. Utalás");
+                Console.WriteLine("4. Utalás két számla között");
                 Console.WriteLine("5. Hitelkeret módosítása");
                 Console.WriteLine("6. Kilépés és Naplózás");
                 Console.Write("\nVálasszon egy menüpontot: ");
@@ -30,6 +35,25 @@ namespace bankszamla_projekt
 
 
             }
-        }      
+        }
+        static void AdatokBetoltese(string fajlnev)
+        {
+            StreamReader sr = new StreamReader(fajlnev);
+
+            while (!sr.EndOfStream)
+            {
+                string sor = sr.ReadLine();
+                string[] adatok = sor.Split(';');
+
+                string szsz = adatok[0];
+                string nev = adatok[1];
+                decimal egyenleg = decimal.Parse(adatok[2]);
+
+                Account uj = new Account(szsz, nev, egyenleg, 0, egyenleg);
+                szamlak.Add(uj);
+            }
+
+            sr.Close();
+        }
     }
 }
